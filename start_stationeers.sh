9e1d324cd2fe891f49927f8d12b64ad69b41e67a
Pauli Jokela <pauli.jokela@didstopia.com>
2020-11-02 08:40:38 +0200

    Fixed issues with startup argument handling

--- start_stationeers.sh
+++ start_stationeers.sh
@@ -49,22 +49,58 @@ else
 	bash /steamcmd/steamcmd.sh +runscript /app/install.txt
 fi
 
+# Remove extra whitespace from startup command
+STATIONEERS_STARTUP_COMMAND=$(echo "$STATIONEERS_SERVER_STARTUP_ARGUMENTS" | tr -s " ")
+
+# Set the game port
+if [ ! -z ${STATIONEERS_GAME_PORT+x} ]; then
+	STATIONEERS_STARTUP_COMMAND="${STATIONEERS_STARTUP_COMMAND} -gameport=${STATIONEERS_GAME_PORT}"
+fi
+
+# Set the query/update port
+if [ ! -z ${STATIONEERS_QUERY_PORT+x} ]; then
+	STATIONEERS_STARTUP_COMMAND="${STATIONEERS_STARTUP_COMMAND} -updateport=${STATIONEERS_QUERY_PORT}"
+fi
+
+# Set the world name
+if [ ! -z ${STATIONEERS_WORLD_NAME+x} ]; then
+	STATIONEERS_STARTUP_COMMAND="${STATIONEERS_STARTUP_COMMAND} -worldname=${STATIONEERS_WORLD_NAME} -loadworld=${STATIONEERS_WORLD_NAME}"
+fi
+
+# Set the auto-save interval
+if [ ! -z ${STATIONEERS_SERVER_SAVE_INTERVAL+x} ]; then
+	STATIONEERS_STARTUP_COMMAND="${STATIONEERS_STARTUP_COMMAND} -autosaveinterval=${STATIONEERS_SERVER_SAVE_INTERVAL}"
+fi
+
+# Set the server name
+if [ ! -z ${STATIONEERS_SERVER_NAME+x} ]; then
+	STATIONEERS_STARTUP_COMMAND="${STATIONEERS_STARTUP_COMMAND} -servername=\"${STATIONEERS_SERVER_NAME}\""
+fi
+
+# Set the server password
+if [ ! -z ${STATIONEERS_SERVER_PASSWORD+x} ]; then
+	STATIONEERS_STARTUP_COMMAND="${STATIONEERS_STARTUP_COMMAND} -password=${STATIONEERS_SERVER_PASSWORD}"
+fi
+
 # Set the working directory
 cd /steamcmd/stationeers || exit
 
 # Run the server
 echo ""
-echo "Starting Stationeers.."
+echo "Starting Stationeers with arguments: ${STATIONEERS_STARTUP_COMMAND}"
 echo ""
 ./rocketstation_DedicatedServer.x86_64 \
-	${STATIONEERS_SERVER_STARTUP_ARGUMENTS} \
-	-gameport=${STATIONEERS_GAME_PORT} \
-	-updateport=${STATIONEERS_QUERY_PORT} \
-	-worldname=${STATIONEERS_WORLD_NAME} \
-	-loadworld=${STATIONEERS_WORLD_NAME} \
-	-autosaveinterval=${STATIONEERS_SERVER_SAVE_INTERVAL} \
-	-servername "${STATIONEERS_SERVER_NAME}" \
-	2>&1 &
+  ${STATIONEERS_STARTUP_COMMAND} \
+  2>&1 &
+# ./rocketstation_DedicatedServer.x86_64 \
+# 	${STATIONEERS_SERVER_STARTUP_ARGUMENTS} \
+# 	-gameport=${STATIONEERS_GAME_PORT} \
+# 	-updateport=${STATIONEERS_QUERY_PORT} \
+# 	-worldname=${STATIONEERS_WORLD_NAME} \
+# 	-loadworld=${STATIONEERS_WORLD_NAME} \
+# 	-autosaveinterval=${STATIONEERS_SERVER_SAVE_INTERVAL} \
+# 	-servername "${STATIONEERS_SERVER_NAME}" \
+# 	2>&1 &
 
 child=$!
 wait "$child"
